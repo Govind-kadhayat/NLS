bf5fab23cc45710038c4ede1659a99fdbce1cd23
Govind Kadhayat <govindkadhayat@gmail.com>
2024-07-23 20:43:42 +0545

    udpate3

--- NLS/NepalLicenseScholar/admin.py
+++ NLS/NepalLicenseScholar/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from NepalLicenseScholar.models import Contact
-from NepalLicenseScholar.models import Signup
+
 
 # Register your models here.
+from .models import *
 
 admin.site.register(Contact)
 admin.site.register(Signup)
+admin.site.register(Category)
+class AnserAdmin(admin.StackedInline):
+    model = Answer
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnserAdmin]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer)
--- NLS/NepalLicenseScholar/models.py
+++ NLS/NepalLicenseScholar/models.py
@@ -1,4 +1,17 @@
 from django.db import models
+import uuid
+
+
+class BaseModel(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)                 
+  
+    class Meta:
+      abstract = True
+
+
+
 
 # Create your models here.
 class Contact(models.Model):
@@ -21,3 +34,20 @@ class Signup(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Category(models.Model):
+   category_name = models.CharField(max_length=100)
+  
+  
+  
+class Question(BaseModel):
+   Category = models.ForeignKey(Category, related_name='category', on_delete=models.CASCADE)
+   question = models.CharField(max_length=100)
+   marks = models.IntegerField(default=5)
+
+
+class Answer(BaseModel):
+    question = models.ForeignKey(Question,related_name='question_answer', on_delete=models.CASCADE)
+    answer = models.CharField(max_length=100)
+    is_correct = models.BooleanField(default=False)
