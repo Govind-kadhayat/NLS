20bbf840ca220f82331f0a2a771fc89d46ed1bb8
Govind Kadhayat <govindkadhayat@gmail.com>
2024-11-14 11:54:37 +0545

    11/14

--- NLS/NepalLicenseScholar/views.py
+++ NLS/NepalLicenseScholar/views.py
@@ -50,7 +50,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('home')
+            return redirect('dashboard')
         else:
             messages.error(request, 'Invalid username or password')
     return render(request, 'login.html')
